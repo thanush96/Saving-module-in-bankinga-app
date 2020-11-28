30fad0997f11df23bb0184925aabf53f6cfacadb
thanush96 <danushthanush@gmail.com>
2020-11-28 12:13:42 +0530

    step 4 :Completed Form Design

--- app/src/main/java/com/example/demofirebasetorecycler/DetailsActivity.java
+++ app/src/main/java/com/example/demofirebasetorecycler/DetailsActivity.java
@@ -8,21 +8,21 @@ import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
     ImageView img;
-    TextView tv1,tv2,tv3,tv4,tv5;
+    TextView tv1, tv2, tv3, tv4, tv5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-       // img=(ImageView)findViewById(R.id.dimg);
-        tv1=(TextView)findViewById(R.id.dname);
-        tv2=(TextView)findViewById(R.id.drate);
-        tv3=(TextView)findViewById(R.id.daddress);
-        tv4=(TextView)findViewById(R.id.dmail);
-        tv5=(TextView)findViewById(R.id.dtp);
+        // img=(ImageView)findViewById(R.id.dimg);
+        tv1 = (TextView) findViewById(R.id.dname);
+        tv2 = (TextView) findViewById(R.id.drate);
+        tv3 = (TextView) findViewById(R.id.daddress);
+        tv4 = (TextView) findViewById(R.id.dmail);
+        tv5 = (TextView) findViewById(R.id.dtp);
 
-      // img.setImageResource(getIntent().getIntExtra("image",0));
+        // img.setImageResource(getIntent().getIntExtra("image",0));
         tv1.setText(getIntent().getStringExtra("name"));
         tv2.setText(getIntent().getStringExtra("rate"));
         tv3.setText(getIntent().getStringExtra("address"));
---
+++ app/src/main/java/com/example/demofirebasetorecycler/form.java
@@ -0,0 +1,5 @@
+package com.example.demofirebasetorecycler;
+
+public class form {
+
+}
