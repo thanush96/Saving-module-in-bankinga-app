c1e8ea405e06f8927be12fb54717c54c295f07e9
thanush96 <danushthanush@gmail.com>
2020-11-28 22:24:12 +0530

    step 5 :Form Datas Store into firebase and some design changes

--- app/src/main/java/com/example/demofirebasetorecycler/DetailsActivity.java
+++ app/src/main/java/com/example/demofirebasetorecycler/DetailsActivity.java
@@ -3,12 +3,25 @@ package com.example.demofirebasetorecycler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.Normalizer;
 
 public class DetailsActivity extends AppCompatActivity {
     ImageView img;
-    TextView tv1, tv2, tv3, tv4, tv5;
+    TextView tv1, tv2, tv3, tv4, tv5,type;
+    Button apply;
+    EditText customername,customeraddress,customernic,customercontact,customermail;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,12 @@ public class DetailsActivity extends AppCompatActivity {
         tv3 = (TextView) findViewById(R.id.daddress);
         tv4 = (TextView) findViewById(R.id.dmail);
         tv5 = (TextView) findViewById(R.id.dtp);
+        type = (TextView) findViewById(R.id.dtype);
+        apply=(Button)findViewById(R.id.btnapply);
+        customername=findViewById(R.id.cname);
+        customeraddress=findViewById(R.id.caddress);
+        customernic=findViewById(R.id.cnic);
+        customercontact=findViewById(R.id.ccontact);
 
         // img.setImageResource(getIntent().getIntExtra("image",0));
         tv1.setText(getIntent().getStringExtra("name"));
@@ -28,6 +47,29 @@ public class DetailsActivity extends AppCompatActivity {
         tv3.setText(getIntent().getStringExtra("address"));
         tv4.setText(getIntent().getStringExtra("mail"));
         tv5.setText(getIntent().getStringExtra("tp"));
+
+        //Save data in firebase on button click
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode=FirebaseDatabase.getInstance();
+                reference=rootNode.getReference("customers");
+
+                //Get and set all data values
+                String name=customername.getText().toString();
+                String address=customeraddress.getText().toString();
+                String nic=customernic.getText().toString();
+                String contact=customercontact.getText().toString();
+
+                String bank=tv1.getText().toString();
+                String rate=tv2.getText().toString();
+                String Type=type.getText().toString();
+
+                form myform=new form(address,contact,name,nic,bank,rate,Type);
+                reference.child(nic).setValue(myform);
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/demofirebasetorecycler/form.java
+++ app/src/main/java/com/example/demofirebasetorecycler/form.java
@@ -1,5 +1,71 @@
 package com.example.demofirebasetorecycler;
 
 public class form {
+String address,contact,name,nic,bank,rate,type;
 
+    public form(String address, String contact, String name, String nic, String bank, String rate, String type) {
+        this.address = address;
+        this.contact = contact;
+        this.name = name;
+        this.nic = nic;
+        this.bank = bank;
+        this.rate = rate;
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
